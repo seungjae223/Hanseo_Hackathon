e6f62177175f4a4b9621994e6454314a9ffb9405
anjaemin164 <anjamin164@gmail.com>
2025-09-23 19:24:32 +0900

    재민 첫 커밋

---
+++ src/Login.jsx
@@ -0,0 +1,35 @@
+import './Login.css';
+
+export default function Login() {
+      return (
+    <div className="login-wrapper">
+      <div className="login-card">
+        <h1 className="login-title">작당모의</h1>
+
+        <label className="login-label" htmlFor="email">
+          한서대학교 웹메일
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="login-input"
+          placeholder=""
+        />
+
+        <label className="login-label" htmlFor="password">
+          비밀번호
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="login-input"
+          placeholder=""
+        />
+
+        <div className="login-links">
+          회원가입 / 비밀번호 찾기
+        </div>
+      </div>
+    </div>
+  );
+}
