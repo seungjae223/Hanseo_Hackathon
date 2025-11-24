7672fbfe821e77d61207dd1c926f9bf5e9c93637
황승재 <joyfull223@naver.com>
2025-11-25 00:52:02 +0900

    수정중

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -5,12 +5,12 @@ function ProtectedPage() {
   const { isLoggedIn, token } = useAuthStore();
 
   if (!isLoggedIn) {
-    return <h2>🚫 접근 불가! 로그인이 필요합니다.</h2>;
+    return <h2>접근 불가! 로그인이 필요합니다.</h2>;
   }
 
   return (
     <div>
-      <h2>✅ 보호된 페이지 (JWT 토큰 확인 완료)</h2>
+      <h2> 보호된 페이지 (JWT 토큰 확인 완료)</h2>
       <p>현재 토큰: {token}</p>
     </div>
   );
